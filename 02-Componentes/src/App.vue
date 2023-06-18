81ac8449c8ae1333a99dec2469ebbb6dcd8077f3
mduranx <mduranx@hiberus.com>
2023-06-18 20:10:32 +0200

    27 - Contenido dinámico por componentes

--- 02-Componentes/src/App.vue
+++ 02-Componentes/src/App.vue
@@ -2,9 +2,10 @@
   <h1>Componentes</h1>
   <hr/>
 
-  <!-- Uso de props -->
   <Card url="https://cdn.svgporn.com/logos/vue.svg?response-content-disposition=attachment%3Bfilename%3Dvue.svg" nombre="Vue.js"/>
-  <Card />
+  <Card url="https://cdn.svgporn.com/logos/google-bard-icon.svg?response-content-disposition=attachment%3Bfilename%3Dgoogle-bard-icon.svg" nombre="Google Bard"/>
+  <Card url="https://cdn.svgporn.com/logos/xcode.svg?response-content-disposition=attachment%3Bfilename%3Dxcode.svg" nombre="X-code"/>
+  <Card url="https://cdn.svgporn.com/logos/react.svg?response-content-disposition=attachment%3Bfilename%3Dreact.svg" nombre="React"/>
 
 </template>
 
