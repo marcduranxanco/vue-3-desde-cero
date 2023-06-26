5593bd56c533c08b960f980c0b115380cd7710df
mduranx <mduranx@hiberus.com>
2023-06-26 18:22:47 +0200

    35 - V-for

--- 03-Directivas/src/App.vue
+++ 03-Directivas/src/App.vue
@@ -3,13 +3,9 @@
     <h1>Directivas</h1>
     <hr/>
 
-    <h3>Tu edad es {{ edad }}</h3>
-    <h2 v-if="edad >= 18 && edad > 0 && edad < 100">Eres mayor de edad</h2>
-    <h2 v-else-if="edad < 18 && edad > 0 && edad < 100">Eres menor de edad</h2>
-    <h2 v-else>La edad no es válida</h2>
-
-    <button @click="add">Add</button>
-    <button @click="subtract">Subtract</button>
+    <ul>
+      <li v-for="(usuario, index) in usuarios" :key="index">{{index }} - {{ usuario }}</li>
+    </ul>
 
   </div>
 </template>
@@ -17,17 +13,8 @@
 <script>
 export default {
   data: () => ({
-    edad: 21
-  }),
-
-  methods: {
-    add() {
-      this.edad++
-    },
-    subtract() {
-      this.edad--
-    },
-  }
+    usuarios: [ "Marc", "Fer", "Carlos" ]
+  })
 }
 </script>
 
