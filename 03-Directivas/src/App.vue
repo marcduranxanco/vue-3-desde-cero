4467c5c21425019e51bedf876264ea9ca04fd909
mduranx <mduranx@hiberus.com>
2023-06-26 17:54:54 +0200

    33 - V-show

--- 03-Directivas/src/App.vue
+++ 03-Directivas/src/App.vue
@@ -2,15 +2,31 @@
   <div>
     <h1>Directivas</h1>
     <hr/>
-    <div v-html="html" ></div>
+
+    <h3>Tu edad es {{ edad }}</h3>
+    <h2 v-show="edad >= 18">Eres mayor de edad</h2>
+    <h2 v-show="edad < 18">Eres menor de edad</h2>
+
+    <button @click="add">Add</button>
+    <button @click="subtract">Subtract</button>
+
   </div>
 </template>
 
 <script>
 export default {
   data: () => ({
-    html: "<h3>Soy un H3</h3>"
-  })
+    edad: 21
+  }),
+
+  methods: {
+    add() {
+      this.edad++
+    },
+    subtract() {
+      this.edad--
+    },
+  }
 }
 </script>
 
