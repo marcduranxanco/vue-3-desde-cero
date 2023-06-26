50d51b725c6bf8dd0e0bd7b1ffdb42a7b5d9dac1
mduranx <mduranx@hiberus.com>
2023-06-26 17:50:56 +0200

    31 - V-text

--- 03-Directivas/src/App.vue
+++ 03-Directivas/src/App.vue
@@ -2,8 +2,19 @@
   <div>
     <h1>Directivas</h1>
     <hr/>
+    <span v-text="titulo"></span>
+    <br/>
+    <span> {{ titulo }} </span>
   </div>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    titulo : "Hola desde vue"
+  })
+}
+</script>
 
 <style scoped>
   div {
