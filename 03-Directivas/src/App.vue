ff8821ff0dc6aaed8ade8c833eea77db03f324c9
mduranx <mduranx@hiberus.com>
2023-06-26 19:15:55 +0200

    38 - Creación formulario básico con prevent del submit

--- 03-Directivas/src/App.vue
+++ 03-Directivas/src/App.vue
@@ -1,10 +1,23 @@
 <template>
   <div>
-    <h1>Directivas</h1>
+    <h1>Formularios</h1>
+    <img width="100" :src="url" />
     <hr/>
 
-    <!-- v-bind tiene el shorthand ":" -->
-    <img :src="url" />
+    <form @submit.prevent="addUser">
+      <div>
+        <label>Nombre<input type="text" placeholder="Nombre" required/></label>
+      </div>
+      <div>
+        <label>Edad<input type="number" min="15" max="100" placeholder="Edad" required/></label>
+      </div>
+      <div>
+        <label>Email<input type="email" placeholder="mail@mail.com" required/></label>
+      </div>
+        
+        <button type="submit">Enviar</button>
+    </form>
+
 
   </div>
 </template>
@@ -13,7 +26,12 @@
 export default {
   data: () => ({
     url: "https://cdn.svgporn.com/logos/google-bard-icon.svg?response-content-disposition=attachment%3Bfilename%3Dgoogle-bard-icon.svg"
-  })
+  }),
+  methods: {
+    addUser() {
+      console.log("User submited")
+    }
+  }
 }
 </script>
 
