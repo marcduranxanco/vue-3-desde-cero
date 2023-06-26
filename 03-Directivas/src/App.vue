a21eeb0f6677b827d643f8c184c9ac3b698817fd
mduranx <mduranx@hiberus.com>
2023-06-26 18:14:17 +0200

    34 - Directivas condicionales

--- 03-Directivas/src/App.vue
+++ 03-Directivas/src/App.vue
@@ -4,8 +4,9 @@
     <hr/>
 
     <h3>Tu edad es {{ edad }}</h3>
-    <h2 v-show="edad >= 18">Eres mayor de edad</h2>
-    <h2 v-show="edad < 18">Eres menor de edad</h2>
+    <h2 v-if="edad >= 18 && edad > 0 && edad < 100">Eres mayor de edad</h2>
+    <h2 v-else-if="edad < 18 && edad > 0 && edad < 100">Eres menor de edad</h2>
+    <h2 v-else>La edad no es válida</h2>
 
     <button @click="add">Add</button>
     <button @click="subtract">Subtract</button>
