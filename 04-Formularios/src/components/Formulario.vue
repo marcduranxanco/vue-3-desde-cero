ce0f31b0fb104ab9e2ddd67c1d901eee74bd911a
mduranx <mduranx@hiberus.com>
2023-06-29 18:39:47 +0200

    41 - Update Form and bootstrap cdn

--- 04-Formularios/src/components/Formulario.vue
+++ 04-Formularios/src/components/Formulario.vue
@@ -1,18 +1,24 @@
 <template>
     <form>
-        <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        <div class="mb-3">
+            <label class="form-label">Proyecto</label>
+            <input type="text" class="form-control" required/>
         </div>
-        <div class="form-group">
-            <label for="exampleInputPassword1">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+
+        <div class="mb-3">
+            <label class="form-label">Actividad</label>
+            <select class="form-select" required>
+                <option disabled selected value="">Seleccione un tipo...</option>
+                <option>Aplicación web con Vue.Js</option>
+                <option>Backend Service con Node.Js</option>
+                <option>App Movil con React Native</option>
+            </select>
         </div>
-        <div class="form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+
+        <div class="mb-3">
+            <label class="form-check-label" for="exampleCheck1">Urgente</label>
+            <input type="checkbox" class="form-check-input">
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-primary">Guardar</button>
     </form>
 </template>
