3a59dfa6c2dd10a08b9bbb86872648a3e6249e18
mduranx <mduranx@hiberus.com>
2023-06-29 17:59:37 +0200

    39 - v-model

--- 04-Formularios/src/App.vue
+++ 04-Formularios/src/App.vue
@@ -6,13 +6,13 @@
 
     <form @submit.prevent="addUser">
       <div>
-        <label>Nombre<input type="text" placeholder="Nombre" required/></label>
+        <label>Nombre<input v-model="nombre" type="text" placeholder="Nombre" required/></label>
       </div>
       <div>
-        <label>Edad<input type="number" min="15" max="100" placeholder="Edad" required/></label>
+        <label>Edad<input v-model="edad" type="number" min="15" max="100" placeholder="Edad" required/></label>
       </div>
       <div>
-        <label>Email<input type="email" placeholder="mail@mail.com" required/></label>
+        <label>Email<input v-model="correo" type="email" placeholder="mail@mail.com" required/></label>
       </div>
         
         <button type="submit">Enviar</button>
@@ -25,11 +25,17 @@
 <script>
 export default {
   data: () => ({
-    url: "https://cdn.svgporn.com/logos/google-bard-icon.svg?response-content-disposition=attachment%3Bfilename%3Dgoogle-bard-icon.svg"
+    url: "https://cdn.svgporn.com/logos/google-bard-icon.svg?response-content-disposition=attachment%3Bfilename%3Dgoogle-bard-icon.svg",
+    nombre: "",
+    edad: 0,
+    correo: ""
   }),
   methods: {
     addUser() {
       console.log("User submited")
+      console.log(this.nombre)
+      console.log(this.edad)
+      console.log(this.correo)
     }
   }
 }
