f82f295b2d7303831c6fc6179d126ad3f19985d4
mduranx <mduranx@hiberus.com>
2023-08-13 17:37:01 +0200

    64 - Creación de componenetes

--- 04-SASS-HTTP/src/App.vue
+++ 04-SASS-HTTP/src/App.vue
@@ -1,11 +1,16 @@
 <template>
    <div class="container">
     <h1>Cotizador de Ciptomonedas</h1>
+    <formulario />
+    <Data />
    </div>
 </template>
 
 <script>
+import Data from './components/Data.vue'
+import Formulario from './components/Formulario.vue'
 export default {
+  components: { Formulario, Data },
 
 }
-</script>
+      </script>
---
+++ 04-SASS-HTTP/src/components/Data.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h2>Data</h2>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
