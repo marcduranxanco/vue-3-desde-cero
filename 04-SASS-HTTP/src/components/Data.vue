f16a4c5f9bfc4f3f6c24068c0c0cd2a0e8dbf8d1
mduranx <mduranx@hiberus.com>
2024-01-04 16:17:45 +0100

    98 - Diseño del apartado Data

--- 04-SASS-HTTP/src/components/Data.vue
+++ 04-SASS-HTTP/src/components/Data.vue
@@ -1,6 +1,18 @@
 <template>
-    <div>
-        <h2>Data</h2>
+    <div class="data">
+        <h2>Información...</h2>
+
+        <h3>
+            <span class="cripto">BTC</span>
+             - 
+            <span class="moneda">USD</span>
+        </h3>
+
+        <img src="https://www.cryptocompare.com/media/37746238/eth.png">
+        <p>
+            Precio: <strong>$ 300</strong>
+        </p>
+
     </div>
 </template>
 
