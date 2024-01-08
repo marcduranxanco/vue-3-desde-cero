d1a0eb5f11578c10d00e841ef292e9cd68a22a80
mduranx <mduranx@hiberus.com>
2024-01-08 19:22:33 +0100

    82 - Añadir información por defecto

--- 04-SASS-HTTP/src/App.vue
+++ 04-SASS-HTTP/src/App.vue
@@ -3,7 +3,12 @@
     <h1>Cotizador de Ciptomonedas</h1>
     <grid>
       <formulario @info-monedas="obtener" />
-      <Data />
+      <Data 
+        :cripto="info.cripto" 
+        :moneda="info.moneda"
+        :img="info.img"
+        :precio="info.precio"
+      />
     </grid>
    </div>
 </template>
@@ -13,13 +18,25 @@ import Data from './components/Data.vue'
 import Formulario from './components/Formulario.vue'
 import Grid from './components/Grid.vue'
 export default {
+  data: () => ({
+    info: {
+      cripto: "*",
+      moneda: "*",
+      img: "",
+      precio: 0
+    }
+  }),
   components: { Formulario, Data, Grid },
   methods: {
         async obtener(cripto, moneda){
             const endpoint = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${encodeURI(cripto)}&tsyms=${encodeURI(moneda)}`;
             const res = await fetch(endpoint);
             const data = await res.json();
-            console.log(data);
+
+            const dataCripto = data.RAW[cripto];
+            const dataMoneda = dataCripto[moneda];
+
+            console.log(dataMoneda);
         }
     }
 }
--- 04-SASS-HTTP/src/components/Data.vue
+++ 04-SASS-HTTP/src/components/Data.vue
@@ -3,14 +3,14 @@
         <h2>Información...</h2>
 
         <h3>
-            <span class="cripto">BTC</span>
+            <span class="cripto">{{ cripto }}</span>
              - 
-            <span class="moneda">USD</span>
+            <span class="moneda">{{ moneda }}</span>
         </h3>
 
-        <img src="https://www.cryptocompare.com/media/37746238/eth.png">
+        <img :src="`https://www.cryptocompare.com${ img }`">
         <p>
-            Precio: <strong>$ 300</strong>
+            Precio: <strong>$ {{ precio }}</strong>
         </p>
 
     </div>
@@ -18,6 +18,23 @@
 
 <script>
 export default {
-
+    props: {
+        cripto: {
+            trype: String,
+            required: true
+        },
+        moneda: {
+            trype: String,
+            required: true
+        },
+        img: {
+            trype: String,
+            required: true
+        },
+        precio: {
+            trype: String,
+            required: true
+        }
+    }
 }
 </script>
